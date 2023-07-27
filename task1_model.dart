6cb8c324866a7aefc2ab651fc208743d2a210656
Alaa Emad <123553752+alaasw01@users.noreply.github.com>
2023-07-27 15:07:00 -0700

    Add files via upload

---
+++ task1_model.dart
@@ -0,0 +1,13 @@
+class ItemModel{
+  String imagePath;
+  String itemName;
+  String  itemShiping;
+  String itemPrice;
+  ItemModel({
+    required this.imagePath,
+    required this.itemName,
+    required this.itemShiping,
+    required this.itemPrice,
+  });
+
+}
